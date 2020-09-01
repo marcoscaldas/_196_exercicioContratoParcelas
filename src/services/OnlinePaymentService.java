06d011e79e328fe7275b93c4befccee1d6ff5fb4
marcoscaldas <donarumo@hotmail.com>
2020-09-01 16:28:08 -0300

    Contrato com parcelas

---
+++ src/services/OnlinePaymentService.java
@@ -0,0 +1,8 @@
+package services;
+
+public interface OnlinePaymentService {
+	
+	double paymentfee(double amount);
+	double interest(double amount, int months); 
+
+}
---
+++ src/services/PaypalService.java
@@ -0,0 +1,23 @@
+package services;
+
+public class PaypalService implements OnlinePaymentService{
+	
+	private static final double PAYMENT_FEE = 0.02;
+	private static final double MONTHLY_INTEREST = 0.01;
+
+	@Override
+	public double paymentfee(double amount) {
+		
+		return amount * PAYMENT_FEE;
+	}
+
+	@Override
+	public double interest(double amount, int months) {
+		// TODO Auto-generated method stub
+		return amount * months * MONTHLY_INTEREST;
+	}
+	
+	
+	
+
+}
